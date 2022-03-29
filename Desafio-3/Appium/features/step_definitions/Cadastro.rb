4081b962dc3f0a11e839d27fc9b35ee63aac84a6
Luzia <Amorim>
2022-03-29 17:21:29 -0400

    automation with Appium
    Signed-off-by: Luzia <Amorim>

---
+++ Desafio-3/Appium/features/step_definitions/Cadastro.rb
@@ -0,0 +1,43 @@
+Given('que eu acesso o sistema como funcionário') do
+    find_element(class:"android.widget.ImageButton")
+  end
+  
+  When('eu clicar no botão adicionar') do
+    find_element(class:"android.widget.ImageButton").click
+  end
+  
+  When('clicar no botão Cadastrar Novo') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('o formulário de cadastro é exibido') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('eu informar os campos') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('clicar no botão salvar') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('uma mensagem é exibida {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('eu entrar na tela de cadastro de clientes') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('não preencher os campos') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('clicar em salvar') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('uma {string} é exibida') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
